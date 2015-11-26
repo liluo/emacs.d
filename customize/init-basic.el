1466dcd25ee01d3b6fcdd03512485e42d54da0f4
liluo <i@liluo.org>
2015-11-26 18:36:14 +0800

    Init `init-basic`

---
+++ customize/init-basic.el
@@ -0,0 +1,27 @@
+;;; Basic Configuration
+
+;; Disable make-backup-files & auto-save-files
+(setq make-backup-files nil
+      delete-auto-save-files t)
+
+;; Disable menu-bar
+(menu-bar-mode -1)
+
+;; Display line-number & column-number
+(global-linum-mode t)
+(setq linum-format "%d "
+      column-number-mode t)
+
+;; Display time
+(display-time-mode t)
+(setq display-time-day-and-date t
+      display-time-24hr-format t)
+(display-time)
+
+;; Asking given format
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Enable ido mode
+(ido-mode t)
+
+(provide 'init-basic)
--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 (add-to-list 'load-path (expand-file-name "customize" user-emacs-directory))
 
+(require 'init-basic)
 (require 'init-elpa)
 (require 'init-package-list)
 
