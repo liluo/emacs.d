c0647032ab844eeaac5c4bbff0eabc82ad5a31d0
liluo <i@liluo.org>
2015-12-09 14:26:13 +0800

    KBD: binding `C-x g` && `C-x M-g` for Magit.

--- customize/init-bindings.el
+++ customize/init-bindings.el
@@ -2,4 +2,8 @@
 
 (global-set-key (kbd "M-c") `comment-or-uncomment-region)
 
+;; Magit
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
 (provide 'init-bindings)
