06f80504714ffcc06946ba1b7db5aab7bddb2f15
liluo <i@liluo.org>
2015-12-10 22:58:51 +0800

    KBD: unbinding `M-c`

--- customize/init-bindings.el
+++ customize/init-bindings.el
@@ -1,7 +1,5 @@
 ;;; Key bindings
 
-(global-set-key (kbd "M-c") `comment-or-uncomment-region)
-
 ;; Magit
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
